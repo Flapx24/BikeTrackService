bf6249da485d8e916f942b1d6552f3f246a6be8e
Flapx24 <bares.gafer23@cadiz.salesianos.edu>
2025-05-30 01:19:07 +0200

    App rename

--- src/main/java/com/example/demo/BikeTrackServiceApplication.java
+++ src/main/java/com/example/demo/BikeTrackServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BikeRouteApiApplication {
+public class BikeTrackServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BikeRouteApiApplication.class, args);
+		SpringApplication.run(BikeTrackServiceApplication.class, args);
 	}
 
 }
